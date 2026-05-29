805ae81aac60c1ca64efbed5af7165ef017ebcf6
Sunny Jadaun <digitalsolutions@digibysr>
2026-05-29 18:01:56 +0530

    new update

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -80,16 +80,18 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
       { property: "og:type", content: "website" },
       { name: "twitter:card", content: "summary" },
       { name: "twitter:site", content: "@Lovable" },
-      {
-        rel: "icon",
-        href: "/favicon.ico", // change this path if your file has a different name or format
-      },
+
     ],
     links: [
       {
         rel: "stylesheet",
         href: appCss,
       },
+      {
+        rel: "icon",
+        href: "/favicon.ico", // change this path if your file has a different name or format
+      },
+
 
     ],
   }),
